76a20ec11e53d3a13bcb9cab25ac2a9ff306dd1e
Kisagi07 <shinkazumi7@gmail.com>
2026-09-26 19:47:07 +0700

    ui : implement toast removal animation

--- src/context/Toaster.tsx
+++ src/context/Toaster.tsx
@@ -9,30 +9,45 @@ const ICONS = {
 
 export const Toaster: Component = () => {
 
-  const [alerts, setAlerts] = createStore<{id: string, message: string}[]>([])
+  const [alerts, setAlerts] = createStore<{ id: string, message: string }[]>([])
+  let toast!: HTMLDivElement
 
   const confirm = (message: string, duration = 3000) => {
     const id = crypto.randomUUID()
     setAlerts((draft) => {
       draft.push({ id, message })
     });
+
+    const remove = () => setAlerts(draft => draft.filter(d => d.id !== id));
     setTimeout(() => {
-      setAlerts(draft => draft.filter(d => d.id !== id))
+      const alert = toast.querySelector<HTMLDivElement>(`[data-toastid="${id}"]`);
+      if (alert) {
+        alert.classList.add("animate-toast-end");
+        alert.addEventListener("animationend", () => {
+          remove()
+        })
+      } else {
+        remove()
+      }
     }, duration)
   }
 
   return (
     <>
     <button class="btn btn-primary" onClick={() => confirm("is it working?")}>Try</button>
-    <div class="toast toast-bottom toast-center">
+    <div class="toast toast-bottom toast-center" ref={toast}>
       <For each={alerts}>
         {(alert) => (
-          <div class="alert alert-success" role="alert">
+          <div data-toastid={alert.id} class="alert alert-success" role="alert">
             <span class="icon-[ph--check-fat-fill]" />
             <span>{alert.message}</span>
           </div>
         )}
-      </For>
+        </For>
+        <div class="alert alert-success" role="alert">
+          <span class="icon-[ph--check-fat-fill]" />
+          <span>Message is here</span>
+        </div>
       </div>
     </>
   )
